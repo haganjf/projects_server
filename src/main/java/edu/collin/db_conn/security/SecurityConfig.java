1f44aa5f17100876327c9fdb987bef745f7e07fe
Jack Hagan <jhagan@collin.edu>
2026-03-07 15:43:26 -0600

    adjust authentication parmeters

--- src/main/java/edu/collin/db_conn/security/SecurityConfig.java
+++ src/main/java/edu/collin/db_conn/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/register","/users/login","/users/confirm").permitAll()
+                        .requestMatchers("/users/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter,
