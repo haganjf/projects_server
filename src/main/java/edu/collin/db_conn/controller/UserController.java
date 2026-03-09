73feff3efffdbb8d3f57fc016cbb97b2cebbcd9c
Jack Hagan <jhagan@collin.edu>
2026-03-09 12:02:07 -0500

    Allow all origins

--- src/main/java/edu/collin/db_conn/controller/UserController.java
+++ src/main/java/edu/collin/db_conn/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(originPatterns = "https://*.onrender.com", allowCredentials = "true" )
+@CrossOrigin(originPatterns = "*", allowCredentials = "true" )
 @RestController
 @RequestMapping("/users")
 public class UserController {
