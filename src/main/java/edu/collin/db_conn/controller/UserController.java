ac5586698309a269087aaa34a13fd8c733830f5a
Jack Hagan <jhagan@collin.edu>
2026-03-09 11:29:28 -0500

    Debug allow

--- src/main/java/edu/collin/db_conn/controller/UserController.java
+++ src/main/java/edu/collin/db_conn/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins= "*", allowCredentials = "true" )
+@CrossOrigin(origins = "*", allowCredentials = "true" )
 @RestController
 @RequestMapping("/users")
 public class UserController {
