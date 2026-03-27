afdb99840438424cb8de8091b11b6683a633adde
Jack Hagan <jhagan@collin.edu>
2026-03-27 15:46:30 -0500

    Fix circular db recursion

--- src/main/java/edu/collin/db_conn/model/Category.java
+++ src/main/java/edu/collin/db_conn/model/Category.java
@@ -1,5 +1,6 @@
 package edu.collin.db_conn.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -11,18 +12,18 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToMany(mappedBy = "category")
+    @JsonIgnore
+    private List<Item> items;
 
     private String name;
 
-    @OneToMany(mappedBy = "id")
-    private List<Category> categories;
-
-    public List<Category> getCategories() {
-        return categories;
+    public List<Item> getItems() {
+        return items;
     }
 
-    public void setCategories(List<Category> categories) {
-        this.categories = categories;
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 
     public String getName() {
